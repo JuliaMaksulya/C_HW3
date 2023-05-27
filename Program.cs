252d535df4e32f5ba25cbef48dfb424c75e02020
Julia <maksulyax^2gmail.com>
2023-05-27 20:07:40 +0300

    HW

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Console.WriteLine("Ведите пятизначное число: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num2;
+// if (num > 9999)
+//     {
+//         if ((num / 10000) == (num % 10) && ((num / 1000 - (num / 10000) * 10) == ((num % 100 - num % 10) / 10)))
+//         {
+//             Console.WriteLine("Палиндром");
+//         }
+//         else Console.WriteLine("Не Палиндром");
+
+//     }
+//  else Console.WriteLine("Число не пятизначное");
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+// Console.WriteLine("Введите число: ");
+// double x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// double y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// double z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// double x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// double y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// double z2 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow ((y2-y1),2) + Math.Pow ((z2 - z1),2));
+// Console.WriteLine(d);
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+// Console.WriteLine("Ведите число: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+
+// while (num >= i )
+// {
+//    Console.WriteLine(Math.Pow(i,3));
+//    i++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+ 
